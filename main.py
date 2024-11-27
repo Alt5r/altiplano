2efc074fb106e138a0ff5841bc2f7ca49fd3f50c
forge-633 <67239820+forge-633@users.noreply.github.com>
2024-11-27 12:54:20 +0000

    before refactor

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 from boid import *
 from tools import *
 from random import randint
+
+import threading
+
 # Initialize Pygame
 pygame.init()
 
 # Screen settings
-screen_width = 1000
-screen_height = 1000
+screen_width = 750
+screen_height = 750
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Moving Ball with Velocity")
 
@@ -19,13 +22,40 @@ BALL_COLOR = (100, 200, 255)
 # Ball settings
 
 boids = []
+boids2 = []
 #ball = boid(screen_width//2, screen_height//2, 3, 2, screen_height, screen_width)
 ball_radius = 5
 #ball_pos = ball.position()  # Start in the center
 
-for i in range(150):
+# splitting into multiple lists for threading so hopefulyl increase perfomance as its not a gpu issue
+
+for i in range(75):
     boids.append(boid(randint(20,1000), randint(20,1000), randint(-2,2), randint(-2,2), screen_height, screen_width))
-print(len(boids))
+for i in range(75):
+    boids2.append(boid(randint(20,1000), randint(20,1000), randint(-2,2), randint(-2,2), screen_height, screen_width))
+
+
+def maind(lst):
+    while True:
+        screen.fill(WHITE)
+        #boid updater area
+        for ball in lst:
+            
+            ball.behaviour(boids)
+            
+            ball_pos = ball.getPosition().parseToInt()
+            #print(ball_pos)
+            # Draw ball
+            pygame.draw.circle(screen, ball.getColour(), ball_pos, ball_radius)
+
+        # Refresh screen
+        pygame.display.flip()
+
+        # Frame rate
+        pygame.time.Clock().tick(60)  # Limit to 60 FPS
+
+t1 = threading.Thread(target=maind, args=(boids,))
+t2 = threading.Thread(target=maind, args=(boids2,))
 
 # Main game loop
 running = True
@@ -40,20 +70,9 @@ while running:
     #ball_pos[0] += ball_velocity[0]
     #ball_pos[1] += ball_velocity[1]
     # Fill screen with white
-    screen.fill(WHITE)
-    #boid updater area
-    for ball in boids:
-        
-        ball.behaviour(boids)
-        
-        ball_pos = ball.getPosition().parseToInt()
-        #print(ball_pos)
-        # Draw ball
-        pygame.draw.circle(screen, ball.getColour(), ball_pos, ball_radius)
-        
     
-    # Refresh screen
-    pygame.display.flip()
-
-    # Frame rate
-    pygame.time.Clock().tick(120)  # Limit to 60 FPS
+    t1.start()
+    t2.start()
+   
+    
+    
